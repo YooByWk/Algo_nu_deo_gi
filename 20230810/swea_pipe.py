1c5ba8486c90ceba0463e0c0a99d84f1302db45a
유병욱 <tls19190@naver.com>
2023-08-11 03:37:39 +0900

    2023/08/11 새벽 파이프 절단

--- 20230810/swea_pipe.py
+++ 20230810/swea_pipe.py
@@ -2,14 +2,14 @@ T = int(input())
 for t in range(1,1+T):
     check = input()
     # print(check[1])
-    st = []
-    k = len(check)
-    tp = -1
-    pi = 0
-    cut = 0
+    st = [] # 스 택인가?
+    k = len(check)  # 괄호지옥
+    tp = -1 # 나름 스택이니까 top 있어야지...
+    pi = 0 # 잘릴 파이프의 수
+    cut = 0 # 누적 절단
     # while len(st)<k:
 
-    for i in range(len(check)):
+    for i in range(len(check)): # 파이프 길이만큼 하려구요
         if check[i] == '(':
             st.append(check[i]) # 아 몰라 일단 append 해
             pi += 1 # 파이프 수량 +1
@@ -20,7 +20,7 @@ for t in range(1,1+T):
                 pi -= 1
                 cut += pi
                 tp -= 1
-            else: #파이프 마감
+            else: #파이프 마감 #한개만 늘어난다.
                 st.pop()
                 pi -= 1
                 cut += 1
@@ -31,4 +31,4 @@ for t in range(1,1+T):
     # print(cut, tp, 'ct and tp')
     # print(pi)
     print(f'#{t} {cut}')
-# ((( () ( () () )) ( () )() )) ( () () )
+# ((( () ( () () )) ( () )() )) ( () () ) #인풋 잘라서 확인하는 용도
